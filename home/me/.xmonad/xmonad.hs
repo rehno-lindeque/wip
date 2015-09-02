2fa377e4f0ece2adfd8a77ab9b2e711749097aaa
Rehno Lindeque <errantkid@gmail.com>
2015-09-02 03:39:04 +0000

    Expanding layout tweak

--- home/me/.xmonad/xmonad.hs
+++ home/me/.xmonad/xmonad.hs
@@ -1,69 +1,81 @@
-{-# LANGUAGE FlexibleContexts, ConstraintKinds, RankNTypes #-}
- 
-import XMonad
-import XMonad.Layout
-import XMonad.Layout.MagicFocus as Magic
-import XMonad.Config.Desktop
-import XMonad.Config.Gnome
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run
-import XMonad.Util.EZConfig
--- import XMonad.StackSet
-import XMonad.Actions.WindowGo
-import System.IO
+{-# LANGUAGE ConstraintKinds       #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RankNTypes            #-}
+{-# LANGUAGE RecordWildCards       #-}
+{-# LANGUAGE TypeSynonymInstances  #-}
 
- 
+import           Data.Ratio
+import           System.IO
+import           XMonad                       as XM
+import qualified XMonad.Actions.WindowGo      as Actions
+import qualified XMonad.Config.Desktop        as Config
+import qualified XMonad.Config.Gnome          as Config
+import qualified XMonad.Hooks.DynamicLog      as Hooks
+import qualified XMonad.Hooks.ManageDocks     as Hooks
+import qualified XMonad.Layout                as Layout
+import qualified XMonad.Layout.Accordion      as Layout
+import           XMonad.Layout.LayoutModifier (LayoutModifier)
+import qualified XMonad.Layout.LayoutModifier as Layout
+import qualified XMonad.Layout.MagicFocus     as Layout
+import qualified XMonad.StackSet              as Stack
+import           XMonad.Util.EZConfig
+import           XMonad.Util.Run
+
 main = do
     h <- xmobarProc
     xmonad
       . setupXmobar h
-      . myBorders 
-      . myKeys 
-      . myHooks 
-      . myLayout 
-      . myMisc 
+      . myBorders
+      . myKeys
+      . myHooks
+      . myLayout
+      . myMisc
       $ myConfig
-        
+
 -- myKeys :: SetupCfg_
-myKeys cfg = cfg 
-  { modMask = mod1Mask 
+myKeys cfg = cfg
+  { modMask = mod1Mask
   -- modMask = mod4Mask   -- super instead of alt (usually Windows key)
   -- modMask = lockMask   -- capslock instead of alt
   -- modMask = controlMask -- control instead of alt
   --
   }
-  `additionalKeysP` windows 
-  `additionalKeysP` productivity 
-  `additionalKeysP` browsers 
-  `additionalKeysP` editors 
+  `additionalKeysP` navigation
+  `additionalKeysP` productivity
+  `additionalKeysP` browsers
+  `additionalKeysP` editors
   `additionalKeysP`
   [
   -- ((mod4Mask, xK_q), spawn "sudo killall trayer" >> restart "xmonad" True)
   -- ((mod4Mask, xK_b), sendMessage ToggleStruts)
   ]
   where
-    windows =
-      [
-      -- [ ("M-j", windows focusDown)
-      -- , ("M-k", windows focusUp  )
+    -- This configuration tries to keep the layout somewhat stable in order to provide a consistent environment
+    navigation =
+      [ ("M-<Space>", sendMessage NextLayout)
+      -- , ("M-j", windows (Stack.focusUp . Stack.swapDown) {->> sendMessage NextLayout-}) --TODO
+      -- , ("M-S-j", windows Stack.focusDown)
+      -- , ("M-k", windows (Stack.focusDown . Stack.swapUp))
+      -- , ("M-S-k", windows Stack.focusDown)
+      -- , ("M-<Tab>", windows (Stack.focusUp . Stack.swapDown))
       ]
     productivity =
       [ ("M-p", spawn "dmenu_run -fn 16 -nb '#333' -l 15 -b") -- dmenu is a quick launcher
-      -- , ("M-p", spawn "yeganesh -x -- -fn 16 -nb '#333' -l 10 -b") -- yeganesh runs dmenu, showing popular selections first 
+      -- , ("M-p", spawn "yeganesh -x -- -fn 16 -nb '#333' -l 10 -b") -- yeganesh runs dmenu, showing popular selections first
       --                                                              -- another set of possible flags: yeganesh -x -- -fn '-*-terminus-*-r-normal-*-*-120-*-*-*-*-iso8859-*'
-      -- , ("M-S-P", spawn "gmrun")   -- gmrun 
+      -- , ("M-S-P", spawn "gmrun")   -- gmrun
       -- , ("M-S-v", spawn "chromium-browser --new-window https://github.com/begriffs/haskell-vim-now#keybindings-and-commands") -- vim haskell cheatsheet
       ]
     browsers =
-      [ ("M-b", raiseBrowser)
+      [ ("M-b", Actions.raiseBrowser)
       -- ("M-c", spawn "chromium")
       ]
-    editors =  
-      [ ("M-e", raiseEditor)
+    editors =
+      [ ("M-e", Actions.raiseEditor)
       -- , ("M-e", spawn "emacs")
-      , ("M-s", runOrRaiseMaster "sublime" (className =? "sublime"))
-      , ("M-y", runOrRaiseMaster "yi" (className =? "yi"))
+      , ("M-s", Actions.runOrRaiseMaster "sublime" (className =? "sublime"))
+      , ("M-y", Actions.runOrRaiseMaster "yi" (className =? "yi"))
       ]
       --, ("M-S-<Return>", spawn $ XMonad.terminal conf)
       --, ("M-C-<Return>", spawn "urxvt -e tmux attach")
@@ -104,11 +116,11 @@ myKeys cfg = cfg
       --, ("M-C-S-<Left>", sendMessage $ Move L)
       --, ("M-C-S-<Up>", sendMessage $ Move U)
       --, ("M-C-S-<Down>", sendMessage $ Move D)
-      
+
       -- -- mod4-[1..9] @@ Switch to window N
       -- [((modMask, k), focusNth i)
       --     | (i, k) <- zip [0 .. 8] [xK_1 ..]]
-      
+
       -- -- [F1..F9], Switch to workspace N
       -- -- shift-[F1..F9], Move client to workspace N
       -- --
@@ -118,17 +130,17 @@ myKeys cfg = cfg
       --     , (f, m) <- [(W.view, 0), (W.shift, shiftMask)
       --     , (copy, shiftMask .|. C-)]]
 
-myConfig = desktopConfig -- gnomeConfig
+myConfig = Config.desktopConfig -- Config.gnomeConfig
 
 
-type SetupCfg_ = 
+type SetupCfg_ = forall l.
   ( LayoutClass l Window
   , Read (l Window)
-  ) 
+  )
   => XConfig l
   -> XConfig l
 
--- type MyLayout  = Choose Tall (Choose (Mirror Tall) Full) 
+-- type MyLayout  = Choose Tall (Choose (Mirror Tall) Full)
 -- type SetupCfg_ = XConfig MyLayout -> XConfig MyLayout
 
 -- myMisc :: SetupCfg_
@@ -136,8 +148,8 @@ myMisc cfg = cfg
   { terminal = "gnome-terminal" -- "urxvt"
   }
 
--- myBorders :: SetupCfg_ 
-myBorders cfg = cfg 
+-- myBorders :: SetupCfg_
+myBorders cfg = cfg
   { borderWidth = 2
   , normalBorderColor = "#000000"
   , focusedBorderColor = "#ffaa00"
@@ -145,18 +157,98 @@ myBorders cfg = cfg
 
 
 -- myHooks :: SetupCfg_
-myHooks cfg = cfg 
+myHooks cfg = cfg
   { startupHook = startup
   -- , manageHook = manageDocks <+> manageHook defaultConfig
   -- , layoutHook = avoidStruts $ layoutHook defaultConfig
   }
 
+data ExpandFocused a = ExpandFocused !(Ratio Dimension) deriving (Show, Read)
+
+instance LayoutModifier ExpandFocused Window where
+  pureModifier (ExpandFocused ratio) srect mst layout =
+    (scaleStack mst, Nothing)
+    where
+      -- tuple operations
+      fromIntegral2 :: (Integral i, Num n) => (i, i) -> (n, n)
+      fromIntegral2 (a0, a1) = (fromIntegral a0, fromIntegral a1)
+
+      sub2 :: Num n => (n, n) -> (n,n) -> (n, n)
+      sub2 (a0,a1) (b0,b1) = (a0 - b0, a1 - b1)
+
+      add2 :: Num n => (n, n) -> (n,n) -> (n, n)
+      add2 (a0,a1) (b0,b1) = (a0 + b0, a1 + b1)
+
+      min2 :: Ord o => (o, o) -> (o,o) -> (o, o)
+      min2 (a0,a1) (b0,b1) = (min a0 b0, min a1 b1)
+
+      max2 :: Ord o => (o, o) -> (o,o) -> (o, o)
+      max2 (a0,a1) (b0,b1) = (max a0 b0, max a1 b1)
+
+      -- Screen coordinates
+      (x0, y0, w, h)                     = (rect_x srect, rect_y srect, rect_width srect, rect_height srect)
+      (x1, y1)                           = (x0, y0) `add2` fromIntegral2 (w,h)
+      delta                              = (floor $ ratio * fromIntegral w, floor $ ratio * fromIntegral h)
+
+      -- screen bounded subtraction / addition
+      bsub :: (Ord n, Num n) => (n, n) -> (n,n) -> (n, n)
+      bsub a b = max2 (fromIntegral2 (x0,y0)) (a `sub2` b)
+
+      badd :: Integral i => (i, i) -> (i,i) -> (i, i)
+      badd a b = min2 (fromIntegral2 (x1, y1)) (a `add2` b)
+
+      -- Rectangle from top-left, bottom-right coordinates
+      -- rectFromCoords tl br = uncurry (uncurry Rectangle $ fromIntegral2 tl) (fromIntegral2 $ br `sub2` tl)
+      rectFromCoords tl br = uncurry (uncurry Rectangle $ fromIntegral2 tl) (fromIntegral2 $ br `sub2` tl)
+
+      -- Expand, contract helpers
+      expand d (win, r@Rectangle {..}) =
+        let rxy = (rect_x, rect_y)
+            rwh = (rect_width, rect_height)
+        in  ( win
+            -- , r
+            , rectFromCoords
+                (fromIntegral2 (rxy `bsub` d))
+                ((rxy `add2` fromIntegral2 rwh) `badd` d)
+            )
+      contract :: (Integral num, Integral num') => (num,num) -> (num',num') -> (window, Rectangle) -> (window, Rectangle)
+      contract (dx,dy) dir (win, r@Rectangle {..}) =
+        let rxy = (rect_x, rect_y)
+            rwh = (rect_width, rect_height)
+        in  ( win
+            , r -- TODO: adjust
+            )
+
+      -- Determine direction to shrink a window in using the coordinates of the focus window
+      contractDirection :: Integral num => (num,num) -> (num,num) -> (window,Rectangle) -> (num,num)
+      contractDirection f0@(fx0, fy0) f1@(fx1, fy1) (_,wr) =
+        let w0@(wx0, wy0) = (rect_x wr, rect_y wr)
+            w1@(wx1, wy1) = (wx0, wx1) `add2` fromIntegral2 (rect_width wr, rect_height wr)
+        in (0, 0) -- TODO
+
+      -- Scale the stack appropriately
+      scaleStack Nothing                 = layout
+      scaleStack (Just Stack.Stack {..}) =
+        let (ups, foc:downs) = splitAt (length up) layout
+            fr = snd foc
+            f0@(fx0, fy0) = (rect_x fr, rect_y fr)
+            f1@(fx1, fy1) = (fx0, fx1) `add2` fromIntegral2 (rect_width fr, rect_height fr)
+            d = ( if fx0 > x0 && fx1 < x1 then fst delta else 2 * fst delta
+                , if fy0 > y0 && fy1 < y1 then snd delta else 2 * snd delta
+                )
+        -- in map (contract d) ups ++ [foc] ++ map (contract d) downs
+        -- in map (contract d <*> contractDirection f0 f1 _ _) ups ++ [expand d foc] ++ map (contract d <*> contractDirection f0 f1 _ _) downs
+        in map (flip (contract d) <*> contractDirection f0 f1) ups ++ [expand d foc] ++ map (flip (contract d) <*> contractDirection f0 f1) downs
+
+expandFocused :: Ratio Dimension -> l a -> Layout.ModifiedLayout ExpandFocused l a
+expandFocused ratio = Layout.ModifiedLayout (ExpandFocused ratio)
+
 -- myLayout :: SetupCfg_
-myLayout cfg = cfg 
+myLayout cfg = cfg
   -- Swaps the windows around, which is annoying
   -- { layoutHook = magicFocus (Tall 1 (3/100) (1/2)) ||| tiled ||| Mirror tiled ||| Full
   -- , handleEventHook = Magic.promoteWarp
-  { layoutHook = tiled ||| Mirror tiled ||| Full
+  { layoutHook = expandFocused (1 / 100) (tiled {-||| Mirror tiled-}) ||| Layout.Accordion ||| Full
   }
   where
     -- default tiling algorithm partitions screen into two panes
@@ -164,20 +256,20 @@ myLayout cfg = cfg
     -- number of windows in the master pane
     nmaster = 1
     -- default proportion of screen occupied by master pane
-    ratio = 2/3      
+    ratio = 2 / 3
     -- percent of screen to increment when resizing
     delta = 5 / 100
 
-xmobarProc :: IO Handle            
+xmobarProc :: IO Handle
 xmobarProc = spawnPipe "/run/current-system/sw/bin/xmobar $HOME/.xmobar/xmobarrc"
 
-setupXmobar h cfg = cfg 
-  { manageHook = manageDocks <+> manageHook cfg
-  , layoutHook = avoidStruts $ layoutHook cfg
-  , logHook = dynamicLogWithPP xmobarPP
-    { ppOutput = hPutStrLn h
-    , ppTitle = xmobarColor "black" "" . shorten 50
-    , ppLayout = const "" -- to disable the layout info on xmobar
+setupXmobar h cfg = cfg
+  { manageHook = Hooks.manageDocks <+> manageHook cfg
+  , layoutHook = Hooks.avoidStruts $ layoutHook cfg
+  , logHook = Hooks.dynamicLogWithPP Hooks.xmobarPP
+    { Hooks.ppOutput = hPutStrLn h
+    , Hooks.ppTitle = Hooks.xmobarColor "black" "" . Hooks.shorten 50
+    , Hooks.ppLayout = const "" -- to disable the layout info on xmobar
     }
   }
 
