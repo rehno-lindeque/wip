1ddd632ac35af5b68624cced52a1f0d2773c9db8
Rehno Lindeque <errantkid@gmail.com>
2015-09-02 03:36:53 +0000

    Hard link dotfiles (not sure if this is a good idea though - might revisit)

--- ln-home.sh
+++ ln-home.sh
@@ -4,29 +4,29 @@ if [ -e ~/.bashrc ]; then
 
 else
 
-  ln -s home/me/.bashrc ~/.bashrc
+  ln ./home/me/.bashrc ~/.bashrc
 
   mkdir -p ~/.config/yi
-  ln home/me/.config/yi/yi.hs ~/.config/yi/yi.hs
+  ln ./home/me/.config/yi/yi.hs ~/.config/yi/yi.hs
 
-  ln -s home/me/.emacs ~/
+  ln ./home/me/.emacs ~/
 
   # Can't soft link .gitconfig
-  ln home/me/.gitconfig ~/
+  ln ./home/me/.gitconfig ~/
 
-  ln -s home/me/.inputrc ~/
+  ln ./home/me/.inputrc ~/
 
   mkdir -p ~/.nixpkgs
-  ln -s home/me/.nixpkgs/yi-custom.nix ~/.nixpkgs/
-  ln -s home/me/.nixpkgs/yi.nix ~/.nixpkgs/
-  ln -s home/me/.nixpkgs/config.nix ~/.nixpkgs/
+  ln ./home/me/.nixpkgs/yi-custom.nix ~/.nixpkgs/
+  ln ./home/me/.nixpkgs/yi.nix ~/.nixpkgs/
+  ln ./home/me/.nixpkgs/config.nix ~/.nixpkgs/
 
-  ln -s home/me/.nviminfo ~/
+  ln ./home/me/.nviminfo ~/
 
-  ln home/me/.vimrc ~/
-  ln home/me/.vimrc.local ~/
+  ln ./home/me/.vimrc ~/
+  ln ./home/me/.vimrc.local ~/
 
   mkdir -p ~/.xmonad
-  ln -s home/me/.xmonad/xmonad.hs ~/.xmonad/
+  ln ./home/me/.xmonad/xmonad.hs ~/.xmonad/
 
 fi
