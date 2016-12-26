9ad494e6cac99165a2efdb8ade9577dc1333599d
Rehno Lindeque <errantkid@gmail.com>
2016-12-26 21:30:39 +0000

    macbookair2013

--- etc/nixos/macbookair2013/services.nix
+++ etc/nixos/macbookair2013/services.nix
@@ -1,9 +1,30 @@
 {
-  ...
+  pkgs
+, ...
 }:
 
 {
   services = {
     openssh.enable = false;  # Use this to start an sshd daemon (to enable remote login)
+
+    # Custom Hardware (TODO: Modularize)
+    # Based on the example at https://github.com/NixOS/nixpkgs/issues/10646#issuecomment-183131248
+    udev.packages =
+      let hw1 = pkgs.writeTextFile
+                  {
+                    name = "hw1-udev-rules";
+                    text = ''
+                           SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="1b7c", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
+                           SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="2b7c", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
+                           SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="3b7c", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
+                           SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="4b7c", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
+                           SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="1807", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
+                           SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="1808", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
+                           SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0000", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
+                           SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0001", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
+                           '';
+                    destination = "/etc/udev/rules.d/20-hw1.rules";
+                  };
+      in [ hw1 ];
   };
 }
--- etc/nixos/services.nix
+++ etc/nixos/services.nix
@@ -100,6 +100,8 @@
 
     # Editors
     emacs.enable = true;
-
   };
+
+    
+
 }
