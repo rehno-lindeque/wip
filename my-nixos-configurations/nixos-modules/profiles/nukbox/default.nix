f5ef1de98c7433b78af40072028d41df66ab9f87
Rehno Lindeque <errantkid@gmail.com>
2022-03-16 15:35:15 -0400

    my-nixos-configurations: nukbox wip

--- my-nixos-configurations/nixos-modules/profiles/nukbox/default.nix
+++ my-nixos-configurations/nixos-modules/profiles/nukbox/default.nix
@@ -8,4 +8,26 @@
     };
     desktop.enable = true;
   };
+
+  boot.loader = {
+    # Use the systemd-boot EFI boot loader as it appears to be very simple
+    systemd-boot.enable = true;
+  };
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/nixos";
+      fsType = "ext4";
+    };
+
+    "/boot" = {
+      device = "/dev/disk/by-label/boot";
+      fsType = "vfat";
+    };
+  };
+
+  swapDevices = [
+    {device = "/dev/disk/by-label/swap";}
+  ];
+
 }
