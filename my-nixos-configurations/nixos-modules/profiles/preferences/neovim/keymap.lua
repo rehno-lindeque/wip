24146cba947477c5c79f93978b65bf20bfca732c
Rehno Lindeque <rehno.lindeque@gmail.com>
2022-05-17 22:23:55 +0000

    neovim: improve simple keymaps

--- my-nixos-configurations/nixos-modules/profiles/preferences/neovim/keymap.lua
+++ my-nixos-configurations/nixos-modules/profiles/preferences/neovim/keymap.lua
@@ -8,16 +8,16 @@ local options = function(additional_options)
 end
 
 -- Remap spacebar as the leader key
-vim.keymap.set('', '<Space>', '<Nop>', options)
+vim.keymap.set('', '<Space>', '<Nop>', options())
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Normal mode: Save when the leader key is hit twice (i.e. spacebar)
-vim.keymap.set('n', '<leader><leader>', '<cmd>w<cr>', options)
+vim.keymap.set('n', '<leader><leader>', '<cmd>w<cr>', options({ desc = "Save" }))
 
 -- Normal mode: Quit via the leader key instead of :
-vim.keymap.set('n', '<leader>q', '<cmd>q<cr>', options)
-vim.keymap.set('n', '<leader>Q', '<cmd>q!<cr>', options)
+vim.keymap.set('n', '<leader>q', '<cmd>q<cr>', options({ desc = "Quit neovim" }))
+vim.keymap.set('n', '<leader>Q', '<cmd>q!<cr>', options({ desc = "Forcibly quit neovim" }))
 
 -- Visual mode: Don't replace the yanked register when pasting in visual mode
-vim.keymap.set({'v', 'x'}, 'p', '"_dP', options)
+vim.keymap.set({'v', 'x'}, 'p', '"_dP', options())
