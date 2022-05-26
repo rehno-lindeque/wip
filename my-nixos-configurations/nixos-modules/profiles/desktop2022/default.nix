130b02e0b8d67ba0c4b0551bef240189e4fdd55a
Rehno Lindeque <rehno.lindeque@gmail.com>
2022-05-26 19:35:06 +0000

    desktop2022: remote builder config

--- my-nixos-configurations/nixos-modules/profiles/desktop2022/default.nix
+++ my-nixos-configurations/nixos-modules/profiles/desktop2022/default.nix
@@ -154,6 +154,15 @@ in {
       };
     };
 
+    # Make this machine a remote nix builder
+    nix.sshServe = {
+      enable = true;
+      protocol = "ssh-ng";
+      keys = [
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDihi25C12vUNxZyxAFVo4lZ4R0bSFmTcNfPQl4mrwNf7116dSMcRilBmkG/x0/G5PRtfz8B+OajtZbK2ivjTwYoDL5+DX50X8jCI4sTjOWBXsw8KcAEu/8NcaIl38tq170YChjUomb3PNqzIvR7fFLAqYxlk01T/42m388WNA2IDTFv1Ex0fkuVOKXnW3ULSZdzLRe7Eh6sSA2qOucue8p+uHgKc9Q9CRhWEkik+iUPO2gTC39LDnMDDtkbeFz6P3R8652kwTSNxV//6FlU0zvvynmxiKjdYUUdWtbkkTZDrH4c5fs6WDem+VfKechS3pvbGQXxcWtYivcgWPDBs9NGyZy0118COhTHF+mgL1jxCu+0Dxfz3/XHS1Efg8rVICI9xjcn2X17ammqWBzsd9navGCXCIJZQQYJSDkU2qUy8anc0834ay88q6wbtcjhXHLmZm/EU+3/B5n54cbTv+zH5EB02dfX/1e7vM1isHvKraKq29HUrY9olmQqf43LjBtE1eoAFXo/tfWDg2aWMvUxXVVYWJ2Q3anyKRlaeN5Mo02uFsusCmRNs7r6lBC0OFbKnkLIG2s0i3BqqVGBV+UctktpmrUZRzhL7o6oiTAhAiKv4ns3B7Yk86JlEW9qkhoysgr4KjsFZD7phg5TDl8ECz+rKT8ZXIRLfXQMOzsOQ== me"
+      ];
+    };
+
     # Enable ssh so that I can work on the desktop remotely
     services.openssh = {
       enable = true;
