69f29a7baf6e2fd984097a277ee28803e4c5e614
Rehno Lindeque <rehno.lindeque@gmail.com>
2025-07-02 04:29:29 +0000

    wip: excise nixpkgs-shim

--- flake.nix
+++ flake.nix
@@ -6,8 +6,6 @@
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     my-dev-shells.url = "path:./my-dev-shells";
     my-nixos-configurations.url = "path:./my-nixos-configurations";
-    # nixpkgs-shim-images.url = "github:rehno-lindeque/nixpkgs-shim-images/fc365e485d98dcc1e8f278654618b8edf3424b03"; # master branch is broken
-    # nixpkgs-shim.url = "github:rehno-lindeque/nixpkgs-shim";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     circuithub-nixos-configurations.url = "git+ssh://git@github.com/circuithub/nixos-configurations.git";
@@ -25,23 +23,15 @@
     my-nixos-configurations.inputs = {
       flake-help.follows = "flake-help";
       home-manager.follows = "home-manager";
-      # nixpkgs-shim.follows = "nixpkgs-shim";
       nixpkgs-stable.follows = "nixpkgs-stable";
       nixpkgs-unstable.follows = "nixpkgs-unstable";
       circuithub-nixos-configurations.follows = "circuithub-nixos-configurations";
     };
-    # nixpkgs-shim.inputs = {
-    #   nixpkgs.follows = "nixpkgs-stable";
-    #   nixpkgs-shim-images.follows = "nixpkgs-shim-images";
-    # };
-    # nixpkgs-shim-images.inputs = {
-    #   nixpkgs.follows = "nixpkgs-stable";
-    # };
   };
 
   outputs = {
     self,
-    # nixpkgs-shim,
+    nixpkgs-stable,
     my-dev-shells,
     my-nixos-configurations,
     ...
@@ -49,6 +39,6 @@
     inherit (my-nixos-configurations) nixosConfigurations;
     inherit (my-dev-shells) devShells;
 
-    # formatter.x86_64-linux = nixpkgs-shim.legacyPackages.x86_64-linux.alejandra;
+    formatter.x86_64-linux = nixpkgs-stable.legacyPackages.x86_64-linux.alejandra;
   };
 }
