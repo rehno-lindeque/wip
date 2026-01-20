9cc2f2a1172c4ebb38cbb393390a0307663dbba2
Rehno Lindeque <rehno.lindeque@gmail.com>
2026-01-20 21:35:02 +0000

    flake: add apple-silicon binary cache

--- my-nixos-configurations/flake.nix
+++ my-nixos-configurations/flake.nix
@@ -35,6 +35,15 @@
     voxinput.inputs.nixpkgs.follows = "nixpkgs-stable";
   };
 
+  nixConfig = {
+    extra-substituters = [
+      "https://nixos-apple-silicon.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nixos-apple-silicon.cachix.org-1:8psDu5SA5dAD7qA0zMy5UT292TxeEPzIz8VVEr2Js20="
+    ];
+  };
+
   outputs = {
     self,
     flake-help,
