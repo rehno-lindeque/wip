27c1614fc1207ac5ed17b2f9d6f491e042203291
Rehno Lindeque <rehno.lindeque@gmail.com>
2026-05-01 03:10:09 +0000

    macbookpro2017: enable ssh access from macbookpro2025

--- my-nixos-configurations/nixos-modules/profiles/macbookpro2017/default.nix
+++ my-nixos-configurations/nixos-modules/profiles/macbookpro2017/default.nix
@@ -199,6 +199,28 @@ in {
       ];
     };
 
+    # Enable ssh so that I can work on the laptop remotely
+    services.openssh = {
+      enable = true;
+      settings.PasswordAuthentication = true; # TODO remove
+      # only accessible via the tailscale ip
+      listenAddresses = [
+        {
+          addr = "100.102.213.117";
+          port = 22;
+        }
+      ];
+    };
+    users.users.me.openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDihi25C12vUNxZyxAFVo4lZ4R0bSFmTcNfPQl4mrwNf7116dSMcRilBmkG/x0/G5PRtfz8B+OajtZbK2ivjTwYoDL5+DX50X8jCI4sTjOWBXsw8KcAEu/8NcaIl38tq170YChjUomb3PNqzIvR7fFLAqYxlk01T/42m388WNA2IDTFv1Ex0fkuVOKXnW3ULSZdzLRe7Eh6sSA2qOucue8p+uHgKc9Q9CRhWEkik+iUPO2gTC39LDnMDDtkbeFz6P3R8652kwTSNxV//6FlU0zvvynmxiKjdYUUdWtbkkTZDrH4c5fs6WDem+VfKechS3pvbGQXxcWtYivcgWPDBs9NGyZy0118COhTHF+mgL1jxCu+0Dxfz3/XHS1Efg8rVICI9xjcn2X17ammqWBzsd9navGCXCIJZQQYJSDkU2qUy8anc0834ay88q6wbtcjhXHLmZm/EU+3/B5n54cbTv+zH5EB02dfX/1e7vM1isHvKraKq29HUrY9olmQqf43LjBtE1eoAFXo/tfWDg2aWMvUxXVVYWJ2Q3anyKRlaeN5Mo02uFsusCmRNs7r6lBC0OFbKnkLIG2s0i3BqqVGBV+UctktpmrUZRzhL7o6oiTAhAiKv4ns3B7Yk86JlEW9qkhoysgr4KjsFZD7phg5TDl8ECz+rKT8ZXIRLfXQMOzsOQ== me"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJkde7PU92uQluyO7OSKTqSoreWxibJhM/cOmpJNAiIC me@macbookpro2025"
+    ];
+    systemd.services.sshd = {
+      # Make sure sshd starts after tailscale so that it can successfully bind to the ip address
+      after = ["tailscaled.service"];
+      wants = ["tailscaled.service"];
+    };
+
     # Cloud password manager
     programs._1password-gui.enable = true;
 
