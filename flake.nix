e6ad8b02654cd27a1a236b76c645de0d0569114f
Rehno Lindeque <rehno.lindeque@gmail.com>
2026-06-05 10:56:06 -0400

    wip: stop exporting nixos configurations

--- flake.nix
+++ flake.nix
@@ -3,9 +3,7 @@
 
   inputs = {
     flake-help.url = "github:rehno-lindeque/flake-help";
-    home-manager.url = "github:nix-community/home-manager/release-25.11";
     my-dev-shells.url = "path:./my-dev-shells";
-    my-nixos-configurations.url = "path:./my-nixos-configurations";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     circuithub-nixos-configurations.url = "git+ssh://git@github.com/circuithub/nixos-configurations.git";
@@ -16,16 +14,8 @@
       nixpkgs.follows = "nixpkgs-stable";
       flake-help.follows = "flake-help";
     };
-    home-manager.inputs.nixpkgs.follows = "nixpkgs-stable";
     my-dev-shells.inputs = {
       nixpkgs-unstable.follows = "nixpkgs-unstable";
-    };
-    my-nixos-configurations.inputs = {
-      flake-help.follows = "flake-help";
-      home-manager.follows = "home-manager";
-      nixpkgs-stable.follows = "nixpkgs-stable";
-      nixpkgs-unstable.follows = "nixpkgs-unstable";
-      circuithub-nixos-configurations.follows = "circuithub-nixos-configurations";
     };
   };
 
@@ -33,10 +23,8 @@
     self,
     nixpkgs-stable,
     my-dev-shells,
-    my-nixos-configurations,
     ...
   }: {
-    inherit (my-nixos-configurations) nixosConfigurations;
     inherit (my-dev-shells) devShells;
 
     formatter.x86_64-linux = nixpkgs-stable.legacyPackages.x86_64-linux.alejandra;
