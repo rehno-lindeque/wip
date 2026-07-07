6cf40d1ffce2076547deab5bba0ed606a6a94862
Rehno Lindeque <rehno.lindeque@gmail.com>
2026-07-07 12:28:32 +0000

    my-nixos-configurations: simplify video tools shell

--- my-nixos-configurations/dev-shells/video-tools/default.nix
+++ my-nixos-configurations/dev-shells/video-tools/default.nix
@@ -1,38 +1,11 @@
 {
-  ffmpeg,
-  lib,
   losslesscut-bin,
   mkShell,
-  shotcut,
-  slurp,
-  stdenv,
   wf-recorder,
-}: let
-  videoEditor =
-    if stdenv.hostPlatform.isAarch64 && stdenv.hostPlatform.isLinux
-    then {
-      package = shotcut;
-      command = lib.getExe shotcut;
-      name = "Shotcut";
-    }
-    else {
-      package = losslesscut-bin;
-      command = lib.getExe losslesscut-bin;
-      name = "LosslessCut";
-    };
-in
+}:
 mkShell {
   packages = [
-    ffmpeg
-    slurp
-    videoEditor.package
+    losslesscut-bin
     wf-recorder
   ];
-
-  shellHook = ''
-    echo "Video tools available: wf-recorder, slurp, ffmpeg, ${videoEditor.name}"
-    echo "Record screen with audio: wf-recorder -a -f recording.mp4"
-    echo "Record region with audio: wf-recorder -a -g '\$(slurp)' -f recording.mp4"
-    echo "Edit video: ${videoEditor.command} recording.mp4"
-  '';
 }
